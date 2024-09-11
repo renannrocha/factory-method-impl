ccc5e403723ffdcb2cd4ca7ad78f56d06238d65d
renannrocha <renanrocha8897@gmail.com>
2024-09-11 19:16:51 -0300

    build: iniciando projeto

---
+++ src/test/java/com/renannrocha/projects/gofPaterns/factoryMethod/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renannrocha.projects.gofPaterns.factoryMethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
